b487ff37b03d66e18153e4eb8f5b95ea54edc2f4
reyhanathabilal <reyhanathabilal@gmail.com>
2023-03-31 22:12:57 +0700

    parallax

--- js/script.js
+++ js/script.js
@@ -11,3 +11,17 @@ document.addEventListener('click', function(e) {
       navbarNav.classList.remove('active');
     }
 })
+
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+  speed: 600,
+  parallax: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
